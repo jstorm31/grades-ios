d885f0eaef524a37db6a0795f66931b97a0e465a
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2019-02-25 08:57:41 +0100

    setup project

---
+++ Classification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Classification
+//
+//  Created by Jiří Zdvomka on 25/02/2019.
+//  Copyright © 2019 jiri.zdovmka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
