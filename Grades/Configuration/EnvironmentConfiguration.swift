ef2775e62f8dd11f12c3b7a788f9c9cf1b0c10a6
Jiří Zdvomka <zdvomka.j@hotmail.cz>
2019-03-01 15:36:37 +0100

    refactor configuration to use multiple files

--- Grades/Configuration/EnvironmentConfiguration.swift
+++ Grades/Configuration/EnvironmentConfiguration.swift
@@ -18,34 +18,49 @@ final class EnvironmentConfiguration {
 	}
 
 	convenience init() {
-		let bundle = Bundle.main
-		let configPath = bundle.path(forResource: "config", ofType: "plist")!
-		let config = NSDictionary(contentsOfFile: configPath)!
-
 		let dict = NSMutableDictionary()
-		if let commonConfig = config["Common"] as? [AnyHashable: Any] {
-			dict.addEntries(from: commonConfig)
-		}
+		
+		let commonConfiguration = PlistDocument(path: "Common.plist").data
+		dict.addEntries(from: commonConfiguration)
 
-		if let environment = bundle.infoDictionary!["ConfigEnvironment"] as? String {
-			if let environmentConfig = config[environment] as? [AnyHashable: Any] {
-				dict.addEntries(from: environmentConfig)
-			}
+		if let environment = Bundle.main.infoDictionary!["ConfigEnvironment"] as? String {
+			let path = "\(environment).plist"
+			let environmentConfiguration = PlistDocument(path: path).data
+			dict.addEntries(from: environmentConfiguration)
 		}
 
 		self.init(dictionary: dict)
 	}
+	
+}
+
+struct Auth {
+	var serverUrl: String = ""
+	var tokenUrl: String = ""
+	var clientId: String = ""
+	var clientSecret: String = ""
+	var clientHash: String = ""
+	var callbackId: String = ""
+	
+	var redirectUri: String {
+		return "\(Bundle.main.bundleIdentifier!)://\(callbackId)"
+	}
 }
 
 protocol NSClassificationConfiguration {
-	var authServerUrl: String { get }
+	var auth: Auth { get }
 }
 
 // swiftlint:disable force_cast
 // Note: for debugging it is better for the app to crash if corresponding key
 //       is missing rather than providing default value
 extension EnvironmentConfiguration: NSClassificationConfiguration {
-	var authServerUrl: String {
-		return config["AuthServerUrl"] as! String
+	var auth: Auth {
+		return Auth(serverUrl: config["AuthorizeUrl"] as! String,
+						tokenUrl: config["TokenUrl"] as! String,
+						clientId: config["ClientId"] as! String,
+						clientSecret: config["ClientSecret"] as! String,
+						clientHash: config["ClientHash"] as! String,
+						callbackId: config["CallbackKey"] as! String)
 	}
 }
