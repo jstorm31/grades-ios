220e45b8cc81eee9a57d3494c545ec0f3b4da6bd
Jiří Zdvomka <zdvomka.j@hotmail.cz>
2019-03-02 18:01:32 +0100

    setup font management

--- Grades/View/UIElements/UIPrimaryButton.swift
+++ Grades/View/UIElements/UIPrimaryButton.swift
@@ -13,9 +13,12 @@ class UIPrimaryButton: UIButton {
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        clipsToBounds = true
         setTitleColor(.white, for: .normal)
-        titleLabel?.font = UIFont(name: "Avenir-Roman", size: 24)
+        titleLabel?.font = UIFontMetrics(forTextStyle: .body)
+            .scaledFont(for: UIFont.Grades.primaryButton)
+		titleLabel?.adjustsFontForContentSizeCategory = true
+
+        clipsToBounds = true
         layer.cornerRadius = frame.size.height / 2
         apply(gradient: UIColor.Theme.primaryGradient)
     }
--- info_preprocess_header.h
+++ info_preprocess_header.h
@@ -1,3 +1,3 @@
 // Auto-generated
 
-#define BUILD_NUMBER 29
+#define BUILD_NUMBER 30
