7f11bffa2f2549f00cfbf8bbc66d6f5501202548
Jiří Zdvomka <zdvomka.j@hotmail.cz>
2019-03-13 22:09:15 +0100

    handle polymorphic JSON value

--- Grades/Model/Entity/Classification.swift
+++ Grades/Model/Entity/Classification.swift
@@ -10,33 +10,14 @@ struct Classification: Decodable {
     var text: [ClassificationText]
     var scope: String?
     var type: String?
-    var rawValue: String?
-    var valueType: String?
+    var valueType: DynamicValueType
+    var value: DynamicValue?
 
     enum CodingKeys: String, CodingKey {
         case text = "classificationTextDtos"
         case scope = "aggregationScope"
         case type = "classificationType"
-        case rawValue = "value"
-        case valueType
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        text = try container.decode([ClassificationText].self, forKey: .text)
-        scope = try container.decodeIfPresent(String.self, forKey: .scope)
-        type = try container.decodeIfPresent(String.self, forKey: .type)
-        valueType = try container.decodeIfPresent(String.self, forKey: .valueType)
-
-        // Value for key .value can be either String? or Int?
-        // TODO: refactor - https://stackoverflow.com/questions/48007761/how-to-handle-partially-dynamic-json-with-swift-codable
-        do {
-            rawValue = try container.decodeIfPresent(String.self, forKey: .rawValue)
-        } catch {
-            let intValue = try container.decode(Int.self, forKey: .rawValue)
-            rawValue = String(intValue)
-        }
+        case value, valueType
     }
 }
 
--- info_preprocess_header.h
+++ info_preprocess_header.h
@@ -1,3 +1,3 @@
 // Auto-generated
 
-#define BUILD_NUMBER 70
+#define BUILD_NUMBER 71
