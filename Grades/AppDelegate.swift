f981044ebae92f6f4326dcfd3efcf3ca6c5094a5
Jiří Zdvomka <jirizdvomka@icloud.com>
2019-05-06 12:32:25 +0200

    fix code signing

--- Grades/AppDelegate.swift
+++ Grades/AppDelegate.swift
@@ -46,6 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let tokenObservable = AppDependency.shared.pushNotificationsService.deviceToken
         let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        Log.debug("\(token)")
         tokenObservable.accept(token)
     }
 }
