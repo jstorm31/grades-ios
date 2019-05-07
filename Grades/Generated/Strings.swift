0dfb2bfbb106579feae395de6ff18a7c1f786d0f
Jiří Zdvomka <jirizdvomka@icloud.com>
2019-05-07 22:49:46 +0200

    fix refresh token test

--- Grades/Generated/Strings.swift
+++ Grades/Generated/Strings.swift
@@ -134,7 +134,7 @@ internal enum L10n {
             internal static let classification = L10n.tr("Localizable", "teacher.students.classification")
             /// Final grade not specified
             internal static let finalGradeEmpty = L10n.tr("Localizable", "teacher.students.finalGradeEmpty")
-            /// Grades
+            /// Evaluation items
             internal static let grading = L10n.tr("Localizable", "teacher.students.grading")
             /// Group
             internal static let group = L10n.tr("Localizable", "teacher.students.group")
--- info_preprocess_header.h
+++ info_preprocess_header.h
@@ -1,3 +1,3 @@
 // Auto-generated
 
-#define BUILD_NUMBER 291
+#define BUILD_NUMBER 292
